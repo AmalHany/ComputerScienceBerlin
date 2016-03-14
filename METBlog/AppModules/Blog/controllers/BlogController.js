36e5183818e56e5dffb2486e37eb13c87c4d5616
Heba <heba.darweesh@student.guc.edu.eg>
2016-03-14 13:22:17 +0200

    UPDATE Blog

--- METBlog/AppModules/Blog/controllers/BlogController.js
+++ METBlog/AppModules/Blog/controllers/BlogController.js
@@ -2,7 +2,7 @@ module.exports = function(app, route, express) {
 
   //get all blogs
   app.get('/blogs', function(req, res){
-    app.models.blog.find({}, function(err, blogs) {
+    app.models.Blog.find({}, function(err, blogs) {
       res.json(blogs);
     });
   });
