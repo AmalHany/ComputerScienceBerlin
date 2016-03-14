9dfc44c1d345f7b3f202e2475f27c13778da0724
George sedky <georgesedky@Georges-MBP.lan>
2016-03-14 22:57:19 +0100

    IMPLEMENT angular routing to certain blogs and passing id to angular

--- METBlog/AppModules/Blog/controllers/BlogController.js
+++ METBlog/AppModules/Blog/controllers/BlogController.js
@@ -4,14 +4,14 @@ module.exports = function(app, route, express) {
   app.get('/blogs', function(req, res){
     app.models.Blog.find({}, function(err, blogs) {
       // fake data to test
-      // blogs = [
-      //   {title: "TITLE ALS", description: "asdbhaksdnansjdaa", user:{f_name: "sam"}},
-      //   {title: "TITLE asdLS", description: "ahaksdnansjdaa", user:{f_name: "jam"}},
-      //   {title: "TLE ALS", description: "asdaksdnansjdaa", user:{f_name: "george"}},
-      //   {title: "TITLE ALS", description: "asdbhaksdnansjdaa", user:{f_name: "am"}},
-      //   {title: "TIE ALS", description: "asdbhasdsdnansjdaa", user:{f_name: "sam"}},
-      //   {title: "TIE AL", description: "asdbhaksdnansjdaa", user:{f_name: "sam"}}
-      // ];
+      blogs = [
+        {id: 1, title: "TITLE ALS", description: "asdbhaksdnansjdaa", user:{f_name: "sam"}},
+        {id: 2, title: "TITLE asdLS", description: "ahaksdnansjdaa", user:{f_name: "jam"}},
+        {id: 3, title: "TLE ALS", description: "asdaksdnansjdaa", user:{f_name: "george"}},
+        {id: 4, title: "TITLE ALS", description: "asdbhaksdnansjdaa", user:{f_name: "am"}},
+        {id: 5, title: "TIE ALS", description: "asdbhasdsdnansjdaa", user:{f_name: "sam"}},
+        {id: 6, title: "TIE AL", description: "asdbhaksdnansjdaa", user:{f_name: "sam"}}
+      ];
       res.json(blogs);
     });
   });
