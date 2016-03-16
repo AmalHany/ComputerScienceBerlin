9a83d60a3fe97c02d4a15581afa4441445f6b165
George sedky <georgesedky@Georges-MacBook-Pro.local>
2016-03-16 12:31:33 +0100

    ADD search posts functionality & CHANGE angular blog http request

--- METBlog/AppModules/Blog/controllers/BlogController.js
+++ METBlog/AppModules/Blog/controllers/BlogController.js
@@ -1,21 +1,21 @@
 module.exports = function(app, route, express) {
 
+  //get a blogs
+  app.get('/blogs/:blog_id', function(req, res){
+    app.models.Blog.findOne({_id: req.params.blog_id})
+                   .populate('posts user')
+                   .exec(function(err, blog) {
+                     if (err) throw err;
+                     res.json(blog);
+                   });
+  });
+
   //get all blogs
   app.get('/blogs', function(req, res){
     app.models.Blog.find({}, function(err, blogs) {
       if (err) throw err;
       res.json(blogs);
     });
-  });
-
-  //get a blogs
-  app.get('/blogs/:blog_id', function(req, res){
-    app.models.Blog.findOne({_id: params.blog_id})
-                   .populate('posts user')
-                   .exec(function(err, blog) {
-                     if (err) throw err;
-                     res.json(blog);
-                   });
   });
 
   return function(req, res, next) {
--- METBlog/public/js/controllers.js
+++ METBlog/public/js/controllers.js
@@ -1,23 +1,22 @@
 var blogApp = angular.module('blogApp', []);
 
-blogApp.controller('blogController', ['$routeParams',
-  function($scope, $routeParams, $http) {
-
+blogApp.controller('blogController',
+  function($scope, $http, $routeParams) {
     $scope.getBlog = function(){
         var config = {
           method: "GET",
-          url: "/blogs",
-          data: {blog_id: $routeParams.blogId},
+          url: '/blogs/' + $routeParams.blogId,
           headers: {"Content-Type": "application/json;charset=utf-8"}
         };
         $http(config).then(function(response) {
+            $scope.searchStr = "";
             $scope.blog = response.data;
         });
     }
 
     $scope.getBlog();
   }
-]);
+);
 
 var homeApp = angular.module('homeApp', []);
 
