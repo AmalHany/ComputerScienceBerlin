740bb0b05eb13a1d16c31a3d72d05bd7df23764a
3mRoshdy <ahmedhroshdy@gmail.com>
2016-03-17 00:01:31 +0100

    Added some items in post.html/Controller

--- Blog/AppModules/Post/controllers/PostController.js
+++ Blog/AppModules/Post/controllers/PostController.js
@@ -19,6 +19,24 @@ module.exports = function(app, route, express) {
                      if (err) throw err;
                      res.json(post);
                    });
+  // (Roshdy)Posting comments on the blog
+  // app.post('/posts',function(req,res){
+
+  //   var comment = req.body.text;
+  //   var newComment = new app.models.Comment({
+  //     text: newComment
+
+  //   });
+  //   newComment.save(function(err){
+  //     if (err) throw err;
+  //   });
+  //   res.sendStatus(200);
+
+  //   });
+  //   app.put('/posts',function(req,res){
+
+  //   });
+ //end roshdy
   });
 
   return function(req, res, next) {
