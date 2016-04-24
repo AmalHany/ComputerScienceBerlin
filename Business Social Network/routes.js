a884ff9ee8f5f3b531b87ac0dadb0a41cc808719
MOSTAFASAMY461 <mostafasamy461@gmail.com>
2016-04-24 14:55:18 +0200

    UPDATE

--- Business Social Network/routes.js
+++ Business Social Network/routes.js
@@ -1,9 +1,9 @@
 module.exports = {
-<<<<<<< HEAD
 
-  'Product': require('./AppModules/Product/controllers/productController')
 
-=======
+  'Product': require('./AppModules/Product/controllers/productController'),
+
+
   'socialRec': require('./AppModules/SocialNetworkInfo/controllers/SocialRecController')
->>>>>>> 86c20d8976f99f925471c0162ba9fff9ee0d8eaa
+
 };
