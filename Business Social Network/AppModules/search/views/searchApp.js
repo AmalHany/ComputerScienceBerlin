819ea437dc541870a2ece8a9175af9bdc3a5ccf3
Youssef Swailem <youssefswailem@gmail.com>
2016-04-24 14:48:20 +0200

    Update product search and add DB file for testing

--- Business Social Network/AppModules/search/views/searchApp.js
+++ Business Social Network/AppModules/search/views/searchApp.js
@@ -1,18 +1,19 @@
 var search = angular.module("searchApp", []);
 //what is this array for?
 
-search.controller('SearchBoxController', function($scope){
+search.controller('SearchBoxController', function($scope, $http){
   //why are there sometimes more arguments in second argument (array?)
 
   var userSearch = $scope.search_term;  //user search is the term the user entered in the search
   $scope.searchTerm = userSearch
+
 
   var test1 = {
     name: "test1 bob",
     price: 200,
     seller: "H&M",
     review: "5 stars",
-    tags: ["clothes", "testtag"]
+    tags: ["blue", "male", "jeans"]
   };
 
   var test2 = {
@@ -20,20 +21,48 @@ search.controller('SearchBoxController', function($scope){
     price: 1200,
     seller: "Zara",
     review: "2 stars",
-    tags: ["clothes again", "testtag once again"]
+    tags: ["red", "female", "sweater"]
   };
 
   var allProducts = [test1, test2];
 
+
   $scope.searchResults = allProducts
+
+  // var searchResults = [];
+  //
+  //
+  // for(var i = 0; i<allProducts.length; i++){
+  //
+  //
+  //   if(allProducts[i].name.indexOf(userSearch) != -1
+  //   || allProducts[i].tags.indexOf(userSearch) != -1){
+  //
+  //     searchResults.push(allProducts[i]);
+  //
+  //   }
+  //
+  // };
+  //
+  // $scope.searchResults = searchResults
+
+
 
 
   //on button click
   $scope.search = function(){
 
-    $scope.searchTerm = userSearch
+  };
 
-  }
+  $scope.customSearchFilter = function(product){
+
+    // return (product.name.indexOf(userSearch) !== -1
+    // || product.tags.indexOf(userSearch) !== -1);
+
+
+
+
+  };
 
 
 
