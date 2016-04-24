dc9153609e4d85fb2947565d952d5b542473d9b6
George sedky <georgesedky@Georges-MacBook-Pro.local>
2016-04-24 19:39:32 +0200

    DOCUMENT social recommendation api

--- Business Social Network/AppModules/SocialNetworkInfo/controllers/SocialRecController.js
+++ Business Social Network/AppModules/SocialNetworkInfo/controllers/SocialRecController.js
@@ -1,28 +1,28 @@
 module.exports = function(app, route, express) {
 
-  var graph = require('fbgraph');
+  var graph = require('fbgraph');// facebook graph api module
 
   app.get('/socialRecs', function(req, res){
-    var access_token = "CAACEdEose0cBACnyOfKvTEpDUCg3pXp4gWszv2cg5KPXpzfqgOwZASfZCm8qZAWpUp3OAC3AuZAvGav8LdKXnH30h3V05e8qb3ElOuDijjvGYLcSKulUZAvUo6dIbQDvm6luOrcO9jZArdhFAk4bPR2oIf5p9xj0UhIrPOwqS9B8rvYCOWl6LZB2lThV7iqpwX8JKc5AwZBZAvlPEdjQQdAjO";
-    var query = "me?fields=age_range,gender,feed{description,message,likes},likes{about,name},location,books{name,description,written_by},movies{genre,name,description},groups{name},television{genre,name},music{name,genre},events{name}";
-    graph.setAccessToken(access_token);
-    graph.get(query, function(err, response) {
-      var tags = getTags(response);
-      
+    var access_token = "CAACEdEose0cBACnyOfKvTEpDUCg3pXp4gWszv2cg5KPXpzfqgOwZASfZCm8qZAWpUp3OAC3AuZAvGav8LdKXnH30h3V05e8qb3ElOuDijjvGYLcSKulUZAvUo6dIbQDvm6luOrcO9jZArdhFAk4bPR2oIf5p9xj0UhIrPOwqS9B8rvYCOWl6LZB2lThV7iqpwX8JKc5AwZBZAvlPEdjQQdAjO";//user facebook access token
+    var query = "me?fields=age_range,gender,feed{description,message,likes},likes{about,name},location,books{name,description,written_by},movies{genre,name,description},groups{name},television{genre,name},music{name,genre},events{name}";// facebook graph api query string
+    graph.setAccessToken(access_token);// perform the query to the session's user account
+    graph.get(query, function(err, response) {// perform a get request to facebook graph api
+      var tags = getTags(response);// process the response json object to extract usefull tags
+
       res.send(tags);
     });
   });
 
   var getTags = function(fbObj){
-    var glossary = require("glossary")({
-          blacklist: ["/","_","&","$","%","★","😻","=","◀","ا"],
-          collapse: true
+    var glossary = require("glossary")({// library to process text and extract usefull keywords
+          blacklist: ["/","_","&","$","%","★","😻","=","◀","ا"],// items to avoid while processing text
+          collapse: true// avoid spliting names as much as possible
     });
 
-    var age = fbObj.age_range.max;
-    var gender = fbObj.gender;
-    var location = glossary.extract(fbObj.location.name);
-    var posts = fbObj.feed.data.map(function(post){
+    var age = fbObj.age_range.max;// extracting age of user
+    var gender = fbObj.gender;// extracting gender of user
+    var location = glossary.extract(fbObj.location.name);// extract location information
+    var posts = fbObj.feed.data.map(function(post){// extract tags from every post
       var obj = {};
       obj.tags = [];
       if(post.message !== undefined)
@@ -34,14 +34,14 @@ module.exports = function(app, route, express) {
         obj.likes = post.likes.length;
       return obj;
     });
-    var likes = fbObj.likes.data.map(function(like){
+    var likes = fbObj.likes.data.map(function(like){// extract tags from every like
       var obj = {};
       obj.tags = like.name.split(" ");
       // if(like.about !== undefined)
       //   obj.tags = obj.tags.concat(glossary.extract(like.about));
       return obj;
     });
-    var books = fbObj.books.data.map(function(book){
+    var books = fbObj.books.data.map(function(book){// extract tags from every book
       var obj = {};
       obj.tags = glossary.extract(book.name);
       if(book.description !== undefined)
@@ -50,7 +50,7 @@ module.exports = function(app, route, express) {
         obj.tags.push(book.written_by);
       return obj;
     });
-    var movies = fbObj.movies.data.map(function(movie){
+    var movies = fbObj.movies.data.map(function(movie){// extract tags from every movie
       var obj = {};
       obj.tags = glossary.extract(movie.name);
       // if(movie.description !== undefined)
@@ -59,19 +59,19 @@ module.exports = function(app, route, express) {
         obj.tags.push(movie.genre);
       return obj;
     });
-    var groups = fbObj.groups.data.map(function(group){
+    var groups = fbObj.groups.data.map(function(group){// extract tags from every group
       var obj = {};
       obj.tags = glossary.extract(group.name);
       return obj;
     });
-    var television = fbObj.television.data.map(function(show){
+    var television = fbObj.television.data.map(function(show){// extract tags from every television item
       var obj = {};
       obj.tags = glossary.extract(show.name);
       if(show.genre !== undefined)
         obj.tags.push(show.genre);
       return obj;
     });
-    var music = fbObj.music.data.map(function(m){
+    var music = fbObj.music.data.map(function(m){// extract tags from every music item
       var obj = {};
       obj.tags = [];
       obj.tags.push(m.name);
@@ -79,43 +79,44 @@ module.exports = function(app, route, express) {
         obj.tags = obj.tags.concat(glossary.extract(m.genre));
       return obj;
     });
-    var events = fbObj.events.data.map(function(e){
+    var events = fbObj.events.data.map(function(e){//extract tags from every event
       var obj = {};
       obj.tags = glossary.extract(e.name);
       return obj;
     });
 
-    var tagList = [];
+    var tagList = [];// create an empty list of tags
 
+    //add extracted information to the final result
     tagList.push(gender);
     tagList.push(age + "");
     tagList = tagList.concat(location);
-    tagList = tagList.concat(music.map(function(m){return m.tags}).reduce(function(prev, curr){
+    tagList = tagList.concat(music.map(function(m){return m.tags}).reduce(function(prev, curr){// aggregate all music items' tags together in one list
       return prev.concat(curr);
     }, []));
-    tagList = tagList.concat(movies.map(function(m){return m.tags}).reduce(function(prev, curr){
+    tagList = tagList.concat(movies.map(function(m){return m.tags}).reduce(function(prev, curr){// aggregate all movie items' tags together in one list
       return prev.concat(curr);
     }, []));
-    tagList = tagList.concat(events.map(function(m){return m.tags}).reduce(function(prev, curr){
+    tagList = tagList.concat(events.map(function(m){return m.tags}).reduce(function(prev, curr){// aggregate all event items' tags together in one list
       return prev.concat(curr);
     }, []));
-    tagList = tagList.concat(television.map(function(m){return m.tags}).reduce(function(prev, curr){
+    tagList = tagList.concat(television.map(function(m){return m.tags}).reduce(function(prev, curr){// aggregate all television items' tags together in one list
       return prev.concat(curr);
     }, []));
-    tagList = tagList.concat(groups.map(function(m){return m.tags}).reduce(function(prev, curr){
+    tagList = tagList.concat(groups.map(function(m){return m.tags}).reduce(function(prev, curr){// aggregate all group items' tags together in one list
       return prev.concat(curr);
     }, []));
-    tagList = tagList.concat(books.map(function(m){return m.tags}).reduce(function(prev, curr){
+    tagList = tagList.concat(books.map(function(m){return m.tags}).reduce(function(prev, curr){// aggregate all book items' tags together in one list
       return prev.concat(curr);
     }, []));
-    tagList = tagList.concat(likes.map(function(m){return m.tags}).reduce(function(prev, curr){
+    tagList = tagList.concat(likes.map(function(m){return m.tags}).reduce(function(prev, curr){// aggregate all like items' tags together in one list
       return prev.concat(curr);
     }, []));
-    tagList = tagList.concat(posts.map(function(m){return m.tags}).reduce(function(prev, curr){
+    tagList = tagList.concat(posts.map(function(m){return m.tags}).reduce(function(prev, curr){// aggregate all post items' tags together in one list
       return prev.concat(curr);
     }, []));
 
-    Array.prototype.unique = function (){
+    Array.prototype.unique = function (){// function to remove duplicate elements in an array
         var r = new Array();
         o:for(var i = 0, n = this.length; i < n; i++){
             for(var x = 0, y = r.length; x < y; x++){
@@ -124,7 +125,7 @@ module.exports = function(app, route, express) {
         return r;
     }
 
-    return tagList.unique();
+    return tagList.unique();// return only unique tags
   };
 
   return function(req, res, next) {
