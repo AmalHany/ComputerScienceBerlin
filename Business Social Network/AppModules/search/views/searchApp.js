6a2a377aeed939a09cb6ddb30b61b05964107465
Youssef Swailem <youssefswailem@gmail.com>
2016-04-24 11:08:01 +0200

    Implement inital search mechanism for products

---
+++ Business Social Network/AppModules/search/views/searchApp.js
@@ -0,0 +1,40 @@
+var search = angular.module("searchApp", []);
+//what is this array for?
+
+search.controller('SearchBoxController', function($scope){
+  //why are there sometimes more arguments in second argument (array?)
+
+  var userSearch = $scope.search_term;  //user search is the term the user entered in the search
+  $scope.searchTerm = userSearch
+
+  var test1 = {
+    name: "test1 bob",
+    price: 200,
+    seller: "H&M",
+    review: "5 stars",
+    tags: ["clothes", "testtag"]
+  };
+
+  var test2 = {
+    name: "test car",
+    price: 1200,
+    seller: "Zara",
+    review: "2 stars",
+    tags: ["clothes again", "testtag once again"]
+  };
+
+  var allProducts = [test1, test2];
+
+  $scope.searchResults = allProducts
+
+
+  //on button click
+  $scope.search = function(){
+
+    $scope.searchTerm = userSearch
+
+  }
+
+
+
+});
--- Business Social Network/public/js/mainApp.js
+++ Business Social Network/public/js/mainApp.js
@@ -1,7 +1,14 @@
-  var mainApp = angular.module('mainApp', [ 'ngRoute']);
-  
+  var mainApp = angular.module('mainApp', [ 'ngRoute', 'searchApp']);
+
   mainApp.config(['$routeProvider',
                     function($routeProvider) {
+
+                      $routeProvider.
+
+                      when('/search', {
+                          templateUrl: '/partials/search/searchBox.html',
+                          controller: 'SearchBoxController'
+                        });
 
                     }
                   ]
