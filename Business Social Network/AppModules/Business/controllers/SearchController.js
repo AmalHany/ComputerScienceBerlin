41d2b0bfaea2cd088ebb14007f766a50ad9280aa
Heba <heba.darweesh@student.guc.edu.eg>
2016-04-25 02:08:12 +0200

    added documentation for searchController.js

--- Business Social Network/AppModules/Business/controllers/SearchController.js
+++ Business Social Network/AppModules/Business/controllers/SearchController.js
@@ -1,13 +1,13 @@
 module.exports = function(app, route, express) {
 
-  
+  //A get route for seach business by category. This function gets the category from the request, and finds all the business who has the same category.
   app.get('/searchBusiness', function(req, res, next){
   	var searchCategory = req.query.category;
   	app.models.Business.find({category: searchCategory}, function (err, business) {
   		if (err) {
   			next(err);
   		}
-
+  		
   		res.json({business: business});
   	})
   });
