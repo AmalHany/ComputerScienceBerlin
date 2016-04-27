8df20d0e2c185f5c1fb5c3a694686a719e55637b
amr <amr.aboelnaga@student.guc.edu.eg>
2016-04-27 03:40:43 +0200

    Amr Ahmed login authentication, And Hana cherif logout

--- Business Social Network/AppModules/Login/Controllers/users.js
+++ Business Social Network/AppModules/Login/Controllers/users.js
@@ -32,7 +32,7 @@ router.post('/register', function(req, res){
 	req.checkBody('username', 'Username is required').notEmpty();
 	req.checkBody('password', 'Password is required').notEmpty();
 	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
-
+//
 	var errors = req.validationErrors();
 
 	if(errors){
