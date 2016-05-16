323948e1b4fda5dc991d937fb88005128db9d2d3
George sedky <georgesedky@Georges-MacBook-Pro-2.local>
2016-05-16 21:17:20 +0200

    MODIFY model indes and routes to point to Business and Transaction modules

--- Business Social Network/models_index.js
+++ Business Social Network/models_index.js
@@ -1,5 +1,7 @@
 module.exports = {
   WishList: require('./AppModules/WishList/models/WishList'),
   Product: require('./AppModules/Product/models/Product'),
-  Message: require('./AppModules/Message/models/Message')
+  Message: require('./AppModules/Message/models/Message'),
+  Business: require('./AppModules/Business/models/Business'),
+  Transaction: require('./AppModules/Transaction/models/Transaction')
 };
--- Business Social Network/routes.js
+++ Business Social Network/routes.js
@@ -1,6 +1,9 @@
 module.exports = {
 
   'socialRecs': require('./AppModules/SocialNetworkInfo/controllers/SocialRecController'),
-  'wishlists': require('./AppModules/WishList/controllers/WishListController')
-
+  'wishlists': require('./AppModules/WishList/controllers/WishListController'),
+  'messages': require('./AppModules/Message/controllers/MessageController'),
+  'users': require('./AppModules/User/controllers/UserController'),
+  'businesses': require('./AppModules/Business/controllers/BusinessController'),
+  'transactions': require('./AppModules/Transaction/controllers/TransactionController')
 };
