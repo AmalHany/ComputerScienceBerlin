9012244e84badc26b4cb7cea88fcab38f4b4294d
George sedky <georgesedky@georges-mbp.speedport_w_723v_1_40_000>
2016-05-19 21:48:57 +0200

    Add sendMessage & get user threads to Message api interface

--- Business Social Network/AppModules/Message/controllers/MessageController.js
+++ Business Social Network/AppModules/Message/controllers/MessageController.js
@@ -1,7 +1,14 @@
 module.exports = function(app, route, express) {
 
+  // send a message
+  app.post('/sendMessage', function(req, res){
 
+  });
 
+  // get all user messages as threads
+  app.get('/messages', function(req, res){
+
+  });
 
   return function(req, res, next) {
       next();
