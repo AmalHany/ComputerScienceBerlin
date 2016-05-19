545aa93533e269112974405fbad6cf53ac45deb1
George sedky <georgesedky@georges-mbp.speedport_w_723v_1_40_000>
2016-05-19 22:15:22 +0200

    Add passport configuration to app.js

--- Business Social Network/app.js
+++ Business Social Network/app.js
@@ -5,10 +5,42 @@ var methodOverride = require('method-override');
 var engines = require('consolidate');
 var _ = require('lodash');
 var sugar = require("sugar")
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 // Create the application.
 var app = express();
 var server = require('http').createServer(app);
+
+// Load the models.
+app.models = require('./models_index');
+// Load the routes.
+var routes = require('./routes');
+
+// authentication configuration
+passport.use(new LocalStrategy({ usernameField: 'email' }, function(username, password, done) {
+    app.models.User.findOne({ email: username }, function (err, user) {
+      if (err) { return done(err); }
+      // Return if user not found in database
+      if (!user) {
+        return done(null, false, {
+          message: 'User not found'
+        });
+      }
+      // Return if password is wrong
+      if (!user.validPassword(password)) {
+        return done(null, false, {
+          message: 'Password is wrong'
+        });
+      }
+      // If credentials are correct, return the user object
+      return done(null, user);
+    });
+  }
+));
+
+// add authentication layer to middleware stack
+app.use(passport.initialize());
 
 // Create realtime socket interface
 app.socketIo = require('socket.io').listen(server);
@@ -26,14 +58,13 @@ app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(__dirname + '/public'));
 
 
+
+
+
+
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost/test_App');
 mongoose.connection.once('open', function() {
-
-  // Load the models.
-  app.models = require('./models_index');
-  // Load the routes.
-  var routes = require('./routes');
 
   _.each(routes, function(controller, route) {
     app.use(route, controller(app, route, express));
