e4ddfc33dbf2556084205a2d7b9c77536dcd4053
HazemElAgaty <hazemelagaty@gmail.com>
2016-05-20 19:54:05 +0200

    ADD Documentation

--- Business Social Network/AppModules/WishList/controllers/WishListController.js
+++ Business Social Network/AppModules/WishList/controllers/WishListController.js
@@ -3,36 +3,35 @@ module.exports = function(app, route, express) {
 
 app.get('/wishlists/:wishlist_id', function(req, res){
 
-    app.models.WishList.findOne({_id: req.params.wishlist_id})
+    app.models.WishList.findOne({_id: req.params.wishlist_id})  //fetching user's wishlist
                    .populate('products')
                    .exec(function(err, wishlists){
                      if (err) throw err;
                      var wishlist = {};
                      wishlist.newArr = [];
-                     wishlist.flag = 0;
+                     wishlist.flag = 0;                      //flag if something got updated in the wishlist's products
                      for(var i in wishlists.products){
                      if(wishlists.last_seen < (wishlists.products)[i].updatedAt)
                      wishlist.flag=1;
                    }
-
-                      while(wishlists.products.length)
+                      while(wishlists.products.length)       //inserting products 3 by 3 inorder to view them in a grid way
                         wishlist.newArr.push(wishlists.products.splice(0,3));
 
-                     res.json(wishlist);
+                     res.json(wishlist);    //returning JSON object containing both the flag and the products array
                    });
 
-    app.models.WishList.findByIdAndUpdate(
+    app.models.WishList.findByIdAndUpdate(   //updating the last_seen of the wishlist to the current time
                      req.params.wishlist_id,
                        { last_seen: new Date().toISOString()},function(err,model){
                        if(err){
                          console.log(err);
                          return res.send(err);
                          }
-                         //return res.json(model);
+
                      });
 
   });
-  app.put('/wishlists/:wishlist_id', function(req, res){
+  app.put('/wishlists/:wishlist_id', function(req, res){    //checks if products have been changed inside the user's wishlist
     app.models.WishList.findOne({_id: req.params.wishlist_id})
                    .populate('products')
                    .exec(function(err, wishlists){
@@ -54,14 +53,12 @@ app.get('/wishlists/:wishlist_id', function(req, res){
 
     });
 
-
-
-  app.delete('/wishlists/:wishlist_id', function(req, res){
+  app.delete('/wishlists/:wishlist_id', function(req, res){  //removing product from a wishlist
   var ObjectId = require('mongoose').Types.ObjectId;
   var wishlist_id = req.body.wishlistID,
    product_id = req.body.productID;
   console.info(wishlist_id);
-  app.models.WishList.findByIdAndUpdate(
+  app.models.WishList.findByIdAndUpdate(  //fetching the user's wishlist and removing a given product from it
     wishlist_id,
    { $pull: { 'products': new ObjectId(product_id) } },function(err,model){
       if(err){
--- Business Social Network/AppModules/WishList/views/wishListApp.js
+++ Business Social Network/AppModules/WishList/views/wishListApp.js
@@ -2,25 +2,27 @@ var wishListApp = angular.module('wishListApp', []);
 
 wishListApp.controller('WishListNotificationController',
   function($scope, $http,$routeParams) {
-    var wishlist_id= '573f4399853cb2ca1affb6dc';
-    $scope.getNotifications = function(){
-
-      var config ={
+    var wishlist_id= '573f4399853cb2ca1affb6dc'; //wishlist_id
+    $scope.getNotifications = function(){   //checks if there are new Notifications meaning if any product
+                                            // in a wishlist has changed
+        var config ={
         method: "PUT",
         url: "/wishlists/" + wishlist_id,
         headers: {"Content-Type": "application/json;charset=utf-8"}
       };
               $http(config).then(function(response) {
-               //console.log(response.data);
-               if(response.data.flag == '1'){
-                 alert('Updated');
-               }
+
+               if(response.data.flag == '1')    //if data has been updated since last_seen the server controller sets the flag to 1
+               $scope.notificationCount = "New Notification";
+               else
+               $scope.notificationCount = "No Notifications";
+
 
 
         });
 
     }
-$scope.getNotifications();
+$scope.getNotifications(); // calling the method
 
 
 
@@ -30,7 +32,7 @@ $scope.getNotifications();
 wishListApp.controller('WishListController',
   function($scope, $http,$routeParams) {
 
-    $scope.getWishList = function(){
+    $scope.getWishList = function(){   //view all products inside the wishlist
 
       var config ={
         method: "GET",
@@ -38,17 +40,14 @@ wishListApp.controller('WishListController',
         headers: {"Content-Type": "application/json;charset=utf-8"}
       };
               $http(config).then(function(response) {
-               //console.log(response.data);
             $scope.wishlist = response.data.newArr;
-
-
         });
 
     }
 
 
 
-    $scope.removeProduct = function(x){
+    $scope.removeProduct = function(x){  //remove a certain product from the wishlist
         console.log(x._id);
         console.log($routeParams.wishlistId);
         var config = {
