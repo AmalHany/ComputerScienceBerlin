26ea052197760e75d39d232aca418b551a88d52c
MOSTAFASAMY461 <mostafasamy461@gmail.com>
2016-05-20 17:02:02 +0200

    update message model

--- Business Social Network/AppModules/Message/models/Message.js
+++ Business Social Network/AppModules/Message/models/Message.js
@@ -5,10 +5,10 @@ var MessageSchema = new mongoose.Schema({
    sent_at: { type: Date, default: Date.now },
    content: { type: String, required: true },
    seen: { type: Boolean, default: false },
-   fromBusiness: { type: Boolean, required: true },
-   toBusiness: { type: Boolean, required: true },
-   fromUser: { type: Schema.Types.ObjectId, ref: 'User', requried: true },
-   toUser: { type: Schema.Types.ObjectId, ref: 'User', requried: true }
+   fromBusiness: { type: String, required: true },
+   toBusiness: { type: String, required: true },
+   // fromUser: { type: Schema.Types.ObjectId, ref: 'User', requried: true },
+   // toUser: { type: Schema.Types.ObjectId, ref: 'User', requried: true }
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
