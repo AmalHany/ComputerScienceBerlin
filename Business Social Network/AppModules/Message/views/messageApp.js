db022f37b2f0c19ce6da57358e3a9296339b5143
MOSTAFASAMY461 <mostafasamy461@gmail.com>
2016-05-20 18:06:32 +0200

    function send message

--- Business Social Network/AppModules/Message/views/messageApp.js
+++ Business Social Network/AppModules/Message/views/messageApp.js
@@ -2,11 +2,56 @@
 var messageApp = angular.module('messageApp', []);
 messageApp.controller('messageCRUD', function($scope, $http){
 
-	
 
+     $scope.getMessage = function(){
+        var config = {
+          method: "GET",
+          url: "/message",
+          headers: {"Content-Type": "application/json;charset=utf-8"}
+        };
+        $http(config).then(function(response) {
+            $scope.messages = response.data;
+        });
+      }
 
+      $scope.sendMessage = function(){
+      	if($scope.fromBusiness !== null
+        && $scope.fromBusiness !== ""
+        && $scope.fromBusiness !== undefined
+        && $scope.toBusiness !== null
+        && $scope.toBusiness !== ""
+        && $scope.toBusiness !== undefined
+        && $scope.content !== null
+        && $scope.content !== ""
+        && $scope.content !== undefined)
+      	{
 
+      		var config = {
+            method: "POST",
+            url: "/message",
+            data: {fromBusiness: $scope.fromBusiness,toBusiness:$scope.toBusiness,content:$scope.content},
+            headers: {"Content-Type": "application/json;charset=utf-8"}
+          };
+          $http(config).then(function(response) {
+              $scope.getMessage();
+              $scope.fromBusiness = null;
+              $scope.toBusiness = null;
+              $scope.content = null;
+          });
+      	}
+      }
 
+      // $scope.removemessage = function(x){
+      //   var config = {
+      //     method: "DELETE",
+      //     url: "/message",
+      //     data: {messageID: x._id},
+      //     headers: {"Content-Type": "application/json;charset=utf-8"}
+      //   };
+      //   $http(config).then(function(response) {
+      //       $scope.getMessage();
+      //   });
+      // }
 
 });
 
--- Business Social Network/public/js/controllers.js
+++ Business Social Network/public/js/controllers.js
@@ -2,30 +2,59 @@
 var messageApp = angular.module('messageApp', []);
 messageApp.controller('messageCRUD', function($scope, $http){
 
-	
 
+     $scope.getMessage = function(){
+        var config = {
+          method: "GET",
+          url: "/message",
+          headers: {"Content-Type": "application/json;charset=utf-8"}
+        };
+        $http(config).then(function(response) {
+            $scope.messages = response.data;
+        });
+      }
 
+      $scope.sendMessage = function(){
+      	if($scope.fromBusiness !== null
+        && $scope.fromBusiness !== ""
+        && $scope.fromBusiness !== undefined
+        && $scope.toBusiness !== null
+        && $scope.toBusiness !== ""
+        && $scope.toBusiness !== undefined
+        && $scope.content !== null
+        && $scope.content !== ""
+        && $scope.content !== undefined)
+      	{
 
+      		var config = {
+            method: "POST",
+            url: "/message",
+            data: {fromBusiness: $scope.fromBusiness,toBusiness:$scope.toBusiness,content:$scope.content},
+            headers: {"Content-Type": "application/json;charset=utf-8"}
+          };
+          $http(config).then(function(response) {
+              $scope.getMessage();
+              $scope.fromBusiness = null;
+              $scope.toBusiness = null;
+              $scope.content = null;
+          });
+      	}
+      }
 
+      // $scope.removemessage = function(x){
+      //   var config = {
+      //     method: "DELETE",
+      //     url: "/message",
+      //     data: {messageID: x._id},
+      //     headers: {"Content-Type": "application/json;charset=utf-8"}
+      //   };
+      //   $http(config).then(function(response) {
+      //       $scope.getMessage();
+      //   });
+      // }
 
 });
 
-
-function getRecommendationProducts($http){
-  var tags = [];
-  var config = {
-    method: "GET",
-    url: '/socialRecs'
-  };
-  return $http(config).then(function(response) {
-    return response.data;
-  });
-};
-
-//usage
-// getRecommendationProducts($http).then(function(response){
-//   $scope.tests = response;
-// });
 
 var search = angular.module("searchApp", []);
 
@@ -146,6 +175,22 @@ search.controller('SearchBoxController', function($scope, $http){
   };
 
 });
+
+function getRecommendationProducts($http){
+  var tags = [];
+  var config = {
+    method: "GET",
+    url: '/socialRecs'
+  };
+  return $http(config).then(function(response) {
+    return response.data;
+  });
+};
+
+//usage
+// getRecommendationProducts($http).then(function(response){
+//   $scope.tests = response;
+// });
 
 var wishListApp = angular.module('wishListApp', []);
 
