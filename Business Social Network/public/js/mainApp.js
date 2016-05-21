c4990e41e70197f8488201fe9157a554d00d6875
mohamedmousa95 <mohamedmousa95@yahoo.com>
2016-05-21 12:28:58 +0200

    #98 adding the route for recommendation app and controller

--- Business Social Network/public/js/mainApp.js
+++ Business Social Network/public/js/mainApp.js
@@ -1,7 +1,11 @@
-var mainApp = angular.module('mainApp', [ 'ngRoute']);
+var mainApp = angular.module('mainApp', [ 'ngRoute','recommendationApp']);
 mainApp.config(['$routeProvider',
                   function($routeProvider) {
-
+                      $routeProvider
+                      .when('/recommendation',{
+                           templateUrl:'/partials/ViewRecommendations/recommendation.html',
+                           controller:'recommendationCtrl'
+                      });
                   }
                ]
 );
