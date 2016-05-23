98f4406ddcb6da74286dafc5c6a82c445b3525c3
Kareem Amer <Kareem3amer48@gmail.com>
2016-05-24 00:39:19 +0200

    adding dummy data in database

--- Business Social Network/test/testing_31-1079.js
+++ Business Social Network/test/testing_31-1079.js
@@ -5,17 +5,17 @@ var db = mongoose.connection;
 
 var WishList= require('./../AppModules/WishList/models/WishList');
 var Product= require('./../AppModules/Product/models/Product');
+var User= require('./../AppModules/User/models/User');
 
+var u1= new User({first_name: "mohamed", last_name: "osama", date_of_birth: "6/10/1995", gender: "male"});
+// var p1 = new Product({Name: "sa3a", Price: "18"});
+// var p2 = new Product({Name: "taba2", Price: "180"});
+// var p3 = new Product({Name: "ma3la2a", Price: "1800"});
+// var p4 = new Product({Name: "kobaya", Price: "180"});
+// var p5 = new Product({Name: "mastara", Price: "18"});
+// var p6 = new Product({Name: "shebak", Price: "10"});
 
-
-var p1 = new Product({Name: "sa3a", Price: "18"});
-var p2 = new Product({Name: "taba2", Price: "180"});
-var p3 = new Product({Name: "ma3la2a", Price: "1800"});
-var p4 = new Product({Name: "kobaya", Price: "180"});
-var p5 = new Product({Name: "mastara", Price: "18"});
-var p6 = new Product({Name: "shebak", Price: "10"});
-
-p1.save(function (err) {
+u1.save(function (err) {
   if (err) {
     console.info("fail1");
   }
@@ -23,66 +23,74 @@ p1.save(function (err) {
     console.info("success");
   }
 });
+// p1.save(function (err) {
+//   if (err) {
+//     console.info("fail1");
+//   }
+//   else{
+//     console.info("success");
+//   }
+// });
 
-p2.save(function (err) {
-  if (err) {
-    console.info("fail2");
-  }
-  else{
-    console.info("success");
-  }
-});
+// p2.save(function (err) {
+//   if (err) {
+//     console.info("fail2");
+//   }
+//   else{
+//     console.info("success");
+//   }
+// });
 
-p3.save(function (err) {
-  if (err) {
-    console.info("fail3");
-  }
-  else{
-    console.info("success");
-  }
-});
-
-
-p4.save(function (err) {
-  if (err) {
-    console.info("fail1");
-  }
-  else{
-    console.info("success");
-  }
-});
-
-p5.save(function (err) {
-  if (err) {
-    console.info("fail2");
-  }
-  else{
-    console.info("success");
-  }
-});
-
-p6.save(function (err) {
-  if (err) {
-    console.info("fail3");
-  }
-  else{
-    console.info("success");
-  }
-});
+// p3.save(function (err) {
+//   if (err) {
+//     console.info("fail3");
+//   }
+//   else{
+//     console.info("success");
+//   }
+// });
 
 
+// p4.save(function (err) {
+//   if (err) {
+//     console.info("fail1");
+//   }
+//   else{
+//     console.info("success");
+//   }
+// });
 
-var w1 = new WishList({products:[p1._id,p2._id,p3._id,p4._id,p5._id,p6._id]}); //'.' for current direc '..' get back
+// p5.save(function (err) {
+//   if (err) {
+//     console.info("fail2");
+//   }
+//   else{
+//     console.info("success");
+//   }
+// });
+
+// p6.save(function (err) {
+//   if (err) {
+//     console.info("fail3");
+//   }
+//   else{
+//     console.info("success");
+//   }
+// });
 
 
-w1.save(function (err) {
-  if (err) {
-    console.log(err);
-    console.info("fail");
-  }
-  else{
-    console.info("success");
-  }
-});
+
+// var w1 = new WishList({products:[p1._id,p2._id,p3._id,p4._id,p5._id,p6._id]}); //'.' for current direc '..' get back
+
+
+// w1.save(function (err) {
+//   if (err) {
+//     console.log(err);
+//     console.info("fail");
+//   }
+//   else{
+//     console.info("success");
+//   }
+// });
 
 mongoose.connect('mongodb://localhost/test_App');
