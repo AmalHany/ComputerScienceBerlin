bf3dcff6ae2dde305a2ef8052eaf1ae82321449d
Youssef Swailem <youssefswailem@gmail.com>
2016-05-23 19:41:03 +0200

    Add function to fetch all products from DB

--- Business Social Network/AppModules/Product/controllers/ProductController.js
+++ Business Social Network/AppModules/Product/controllers/ProductController.js
@@ -1,7 +1,12 @@
 module.exports = function(app, route, express) {
 
-
-
+  //get all products
+  app.get('/products', function(req, res){
+    app.models.Product.find({}, function(err, products){
+      if (err) throw err;
+      res.json(products);
+    });
+  });
 
   return function(req, res, next) {
       next();
