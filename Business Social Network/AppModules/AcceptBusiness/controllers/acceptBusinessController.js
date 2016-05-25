f196a327797ead9cd8dceaa825bc5f43a0b3fdc8
Kareem Amer <Kareem3amer48@gmail.com>
2016-05-26 00:12:09 +0200

    #80 added documentaion

--- Business Social Network/AppModules/AcceptBusiness/controllers/acceptBusinessController.js
+++ Business Social Network/AppModules/AcceptBusiness/controllers/acceptBusinessController.js
@@ -2,13 +2,14 @@ module.exports = function(app, route, express) {
 	var Business = require('../models/business');
   var User=app.models.user;
   
-
+//showing all businesses with option to accept
   app.post('/show', function(req, res){
     var x=app.models.Business.db.collection('businesses');
     x.find().toArray(function(err,items){
       res.json(items);
        });
 });
+  //this is for showing accepted businessses
   app.get('/show2', function(req, res){
 
     var arry=[];
@@ -23,12 +24,12 @@ module.exports = function(app, route, express) {
        });
 
 });
-
+// this is for getting user name to ensure admins login
     app.get('/getuser', function(req, res){
 
    res.json(req.user.username);
 });
-
+// accepting business altering approved attribute to true in database 
    app.post('/accept', function(req, res){
     var x=app.models.Business.db.collection('businesses');
     var y=app.models.Business;
