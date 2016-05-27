80fd51944e5c12b2fe16f19baeb922828ccae003
FreakFrank <kareem.ismailmohamed@gmail.com>
2016-05-27 19:20:26 +0200

    #94 sending reportl routes file

--- Business Social Network/routes.js
+++ Business Social Network/routes.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'socialRec': require('./AppModules/SocialNetworkInfo/controllers/SocialRecController')
+  'socialRec': require('./AppModules/SocialNetworkInfo/controllers/SocialRecController'),
+  'report' : require('./AppModules/SendReport/controllers/SendReportController')
 };
