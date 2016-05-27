99a733c9b183a9e840a55637573ee91130a0c8fe
FreakFrank <kareem.ismailmohamed@gmail.com>
2016-05-27 19:26:33 +0200

    #94 sending report models main App file

--- Business Social Network/public/js/mainApp.js
+++ Business Social Network/public/js/mainApp.js
@@ -1,7 +1,8 @@
-var mainApp = angular.module('mainApp', [ 'ngRoute']);
+var mainApp = angular.module('mainApp', ['ngRoute','myApp']);
 mainApp.config(['$routeProvider',
                   function($routeProvider) {
-
-                  }
-               ]
-);
+                  	$routeProvider.when('/sendReport', {
+                  		templateUrl : '/partials/SendReport/index.html',
+                  		controller : 'myCtrl'
+                  	});
+                  }]);
